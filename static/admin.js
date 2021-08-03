00e5ca01d2a3241c14bfbaaa70fb201bcd8086eb
piyush999-o <77581601+piyush999-o@users.noreply.github.com>
2021-08-03 16:53:25 +0530

    Delete static directory

--- static/admin.js
+++
@@ -1,7 +0,0 @@
-let pass = prompt('Enter the Password of the Admin');
-if (pass == 'rameshpan') {
-    alert('You are the Admin. WELCOME TO IN YOUR OWN WEBSITE');
-}
-else {
-    alert('You are not the Admin');
-}
